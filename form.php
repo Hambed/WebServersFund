9aa0a27dc536b00dac33ccd0c3dbaa372d91c2d3
Hamilton Bird <hjwbirdprofessional@gmail.com>
2023-10-14 10:55:33 -0400

    Update form.php

--- form.php
+++ form.php
@@ -31,7 +31,7 @@
         <?php echo"it's fucking incredible that someone in your state can move a mouse and read a form because you're so drunk you're bouta die. "; ?>
         <?php } ?>
     <?php echo"\r\n"; ?>
-    <?php echo"$_POST["Reflection"] "; ?>
+    <?php echo"{$_POST["Reflection"]} "; ?>
     <?php echo"\r\n"; ?>
     <?php echo"Jeez if I though you were sober enough to reflect that much I would have never asked."; ?>
 </div>
