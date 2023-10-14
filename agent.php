c99be0df3cfd15002d6e0a02368a0b0ab81e8a27
Hamilton Bird <hjwbirdprofessional@gmail.com>
2023-10-14 07:31:24 -0400

    Update agent.php

--- agent.php
+++ agent.php
@@ -3,7 +3,7 @@
 <head>
     <title>I KNOW YOUR BROWSER</title>
 </head>
-<body style="background-color:black;">
-    <p style="color:#9e029e; font-size: xxx-large; font-family: garamond;"><?= var_dump($_SERVER) ?></p>
+<body style="background-color:purple;">
+    <p><?= var_dump($_SERVER) ?></p>
 </body>
 </html>
