f1da3ad157b9ba5c50fafaaa4979aa436c62ce7c
Hamilton Bird <hjwbirdprofessional@gmail.com>
2023-11-29 03:52:28 -0500

    Update Control_LED.php

--- Control_LED.php
+++ Control_LED.php
@@ -3,9 +3,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['logic'])) {
         $logic = $_POST['logic'];
 
-        if ($logic === 'on') {
+        if ($logic === 'ON') {
             shell_exec("gpio write 3 1");
-        } elseif ($logic === 'off') {
+        } elseif ($logic === 'OFF') {
             shell_exec("gpio write 3 0");
         }
     }
