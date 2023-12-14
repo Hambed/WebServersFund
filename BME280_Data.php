3c6f15bb8264b1a34272b040dba31abe4f575840
Hamilton Bird <hjwbirdprofessional@gmail.com>
2023-12-14 02:08:33 -0500

    asd

--- BME280_Data.php
+++ BME280_Data.php
@@ -1,12 +1,5 @@
 <?php
 $raw = `python code.py`;
 $deserialized = json_decode($raw, true);
-
-if ($deserialized !== null && isset($deserialized["temperature"]) && isset($deserialized["pressure"]) && isset($deserialized["altitude"])) {
-    echo "Temperature = " . $deserialized["temperature"] . "<br>" .
-         "Pressure = " . $deserialized["pressure"] . "<br>" .
-         "Altitude = " . $deserialized["altitude"] . "<br>";
-} else {
-    echo "Error: Invalid sensor data format.";
-}
+echo "$deserialized["temperature"],$deserialized["pressure"],$deserialized["altitude"]";
 ?>
