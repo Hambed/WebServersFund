c97052ab14d2572e3bb62a1864387aa3af1bf22c
Hamilton Bird <hjwbirdprofessional@gmail.com>
2024-01-12 12:48:33 -0500

    Update malicious.php

---
+++ malicious.php
@@ -0,0 +1,25 @@
+<?php
+$server = "localhost";
+$username = "php";
+$password = "password";
+$database = "AlcoholicsOnymous";
+$conn = mysqli_connect($server, $username, $password, $database);
+if (!$conn) { 
+    die("Connection failed: " . mysqli_connect_error());
+}
+if (isset(htmlspecialchars($_POST["search"]))) {
+    $search = htmlspecialchars($_POST["search"]);
+
+
+
+    if(isset($_SERVER['REMOTE_ADDR'])) {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    
+    $sql = "INSERT INTO malicious (IP, SearchTerm) VALUES ('$ip', '$search')";
+    mysqli_query($conn, $sql);
+
+    $googlesearch = "https://www.google.com/search?q=$search";
+    header('Location: '$googlesearch'');
+}
+?>
